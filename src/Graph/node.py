1f826d76d696e4cb5a1cd9cfe61a1b4e480f9665
Diogoesteves11 <diogojfe@outlook.com>
2024-12-19 22:19:17 +0000

    Image generated

--- src/Graph/node.py
+++ src/Graph/node.py
@@ -2,11 +2,12 @@ from Types.supply import Supply
 from Types.area import Area
 
 class Node: 
-    def __init__(self, id, area: Area, needs):
+    def __init__(self, id, area: Area, needs, afected):
         self.id = id
         self.name = str(area.getAreaName())
         self.area = area
         self.needs = needs
+        self.afected = afected
     
     def setId(self, id):
         self.id = id
@@ -28,5 +29,8 @@ class Node:
     
     def getLongitude(self):
         return self.area.getLongitude()
+    
+    def getAfected(self):
+        return self.afected
 
         
