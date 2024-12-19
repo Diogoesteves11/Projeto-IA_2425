87fbfb94ad91b20caa1de47cc91f8cf8192e4093
RodrigoOFernandes <a104175@uminho.pt>
2024-12-19 22:24:36 +0000

    interface integrada na main

--- src/Interface/aidmatrix.py
+++ src/Interface/aidmatrix.py
@@ -8,9 +8,11 @@ customtkinter.set_appearance_mode("Dark")  # Modos: "System" (padrão), "Dark", 
 customtkinter.set_default_color_theme("blue")  # Temas: "blue" (padrão), "green", "dark-blue"
 
 class App(customtkinter.CTk):
-    def __init__(self):
+    def __init__(self, graph = None):
         super().__init__()
 
+
+        self.graph = graph
         # Configurações da janela
         self.title("CustomTkinter complex_example.py")
         self.geometry("1440x900")
@@ -31,26 +33,18 @@ class App(customtkinter.CTk):
         self.sidebar_button_1 = customtkinter.CTkButton(self, text="Mostrar Grafo", command=self.show_graph, width=200, height=50)
         self.sidebar_button_1.grid(row=1, column=0, columnspan=2, padx=20, pady=10)  # Centraliza os botões
         
-        self.sidebar_button_2 = customtkinter.CTkButton(self, text="Visualizar Algoritmos", command=self.sidebar_button_event, width=200, height=50)
+        self.sidebar_button_2 = customtkinter.CTkButton(self, text="Visualizar Algoritmos", width=200, height=50)
         self.sidebar_button_2.grid(row=2, column=0, columnspan=2, padx=20, pady=10)  # Centraliza os botões
         
-        self.sidebar_button_3 = customtkinter.CTkButton(self, text="Resultados Históricos", command=self.sidebar_button_event, width=200, height=50)
+        self.sidebar_button_3 = customtkinter.CTkButton(self, text="Resultados Históricos", width=200, height=50)
         self.sidebar_button_3.grid(row=3, column=0, columnspan=2, padx=20, pady=10)  # Centraliza os botões
 
         # Criação de um rótulo para exibir a imagem quando o botão for clicado
         self.image_label = customtkinter.CTkLabel(self, text="", image=None)  # Inicialmente vazio
         self.image_label.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="n")  # Ajustar a posição da imagem
 
-    def sidebar_button_event(self):
-        # Alterar o conteúdo na área principal quando o botão é clicado
-        # Aqui você pode adicionar lógica para outros botões
-        pass
 
     def show_graph(self):
-        # Carregar e exibir a imagem na área principal
-        self.img = customtkinter.CTkImage(light_image=Image.open('grafo.png'), dark_image=Image.open('grafo.png'), size=(800, 600))
-        self.image_label.config(image=self.img, text="")  # Atualiza o rótulo com a imagem
+        pass
 
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from Graph.graph import Graph
-
+from Interface.aidmatrix import App
 def main():
     g = Graph()
     
     g.createGraph()
 
     g.draw()
-
+    
+    app = App()
+    
+    app.mainloop()
+    
 
 if __name__ == "__main__":
     main()
